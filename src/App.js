31355879c43d95ac0c1fe514e4de53b6087d0b15
Andres Felipe Murcia Zapata <101027787+andresfmurciaz@users.noreply.github.com>
2023-08-07 15:33:00 -0500

    primer component header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Header from './componentes/Header/Header';
+
+function App() 
+{
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         hola mundo amigo mio
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+     <Header></Header>
+      
     </div>
   );
 }
---
+++ src/componentes/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header(){
+
+    return <header>
+        <img src='/img/hder.png' alt='esto es el header'/>
+    </header>
+    
+      
+}
+
+export default Header
